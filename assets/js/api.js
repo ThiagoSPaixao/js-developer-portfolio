0fca7aec3790022ffa12853645f06c0cdacb2fcd
ThiaguinhoxD <thiaguinhoescolhido26@gmail.com>
2024-03-13 14:06:32 -0300

    Remover todos os arquivos do repositório

--- assets/js/api.js
+++
@@ -1,7 +0,0 @@
-
-
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/ThiagoSPaixao/js-developer-portfolio/main/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
